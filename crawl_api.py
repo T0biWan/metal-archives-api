37eda25a51fcd80a1b44207cf4b638c07bef2e5f
t0biwan <tobi.klatt@web.de>
2018-01-09 14:41:59 +0100

    added cleaning of invalid ids

--- crawl_api.py
+++ crawl_api.py
@@ -2,8 +2,8 @@ from metal_archive_api import *
 
 bands = []
 
-for i in range(51, 52):
+for i in range(1, 527):
     print("Band id: ", i)
     bands.append(process_band_from_id(i))
 
-write_json_to_file("bands.json", bands)
+write_json_to_file("cleaned_bands.json", remove_invalid_ids(bands))
